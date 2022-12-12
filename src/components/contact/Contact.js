cb854cf334c80105e065e8b9beec9d8f7b367285
Moloko Mokubedi <molavovic@gmal.com>
2022-12-12 16:42:19 +0200

    Contact links updates

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -5,7 +5,6 @@ import { Bounce } from 'react-reveal'
 import ContactForm from '../contactForm/ContactForm'
 import linkedin from '../../images/social/linkedin.png'
 import github from '../../images/social/github.png'
-import facebook from '../../images/social/facebook.png'
 
 const Contact = () => {
   return (
@@ -13,27 +12,19 @@ const Contact = () => {
       <ContactForm />
       <Bounce cascade>
         <div className="links">
-          <a
-            href="https://github.com/jigalin"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={github} alt="Github Logo" width="40px" />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/mjigalin/"
+        <a
+            href="https://www.linkedin.com/in/moloko-mokubedi-b6a2a298"
             target="_blank"
             rel="noopener noreferrer"
           >
             <img src={linkedin} alt="Linkedin Logo" width="40px" />
           </a>
-
           <a
-            href="https://www.facebook.com/matthew.jigalin/"
+            href="https://github.com/LokoMoloko98/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img src={facebook} alt="Facebook Logo" width="40px" />
+            <img src={github} alt="Github Logo" width="40px" />
           </a>
         </div>
       </Bounce>
